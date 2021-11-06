0bc584d5f08442f397c4883cbf656731c3e1b35d
clecho52 <nikolayordanov2667@gmail.com>
2021-11-06 19:08:58 +0200

    Added texture to lighting tower

--- scripts/electricArc.js
+++ scripts/electricArc.js
@@ -1,3 +1,5 @@
+lightingPic = new Image();
+lightingPic.src = "lighting.png";
 class arc {
   source = null;
   range = 0;
@@ -8,26 +10,32 @@ class arc {
     this.range = range;
     this.nodes = source.enemiesInRange;
     this.nodes.unshift(source);
-    console.log(source);
   }
   updateArc() {
     if (updates < this.updatesAtStart + 15) {
       for (let i = 1; i < this.nodes.length; i++) {
         this.nodes[i].health -= 1;
+        money += 1;
       }
     } else {
       arcs.splice(arcs.indexOf(this), 1);
     }
   }
   drawSelf() {
-    context.lineWidth = 5;
     for (let i = 1; i < this.nodes.length; i++) {
-      drawLine(
-        this.nodes[i - 1].x,
-        this.nodes[i - 1].y,
-        this.nodes[i].x,
-        this.nodes[i].y
+      let rotationToNext = Math.atan2(
+        this.nodes[i].y - this.nodes[i - 1].y,
+        this.nodes[i].x - this.nodes[i - 1].x
       );
+      let distToNext = Math.sqrt(
+        Math.pow(this.nodes[i - 1].x - this.nodes[i].x, 2) +
+          Math.pow(this.nodes[i - 1].y - this.nodes[i].y, 2)
+      );
+      context.save();
+      context.translate(this.nodes[i - 1].x, this.nodes[i - 1].y);
+      context.rotate(rotationToNext);
+      context.drawImage(lightingPic, 0, 0, distToNext, 10);
+      context.restore();
     }
   }
 }
